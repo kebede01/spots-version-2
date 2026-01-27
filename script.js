ed3ba8c4de370c7c85ff0e1722d321aa6b976c79
kebede01 <johnzeeze@yahoo.com>
2026-01-26 19:09:55 -0800

    Add hover styles to modal css

---
+++ script.js
@@ -0,0 +1,4 @@
+// const profileEditModal = document.querySelector("#edit-modal");
+// const profilePostModal = document.querySelector("#post-modal");
+// const modalInputs = profileEditModal.querySelectorAll(".modal__input");
+// console.log(modalInputs);
