bb5049f8c88fbb207ae73c1a85e9efb18b90c108
kebede01 <johnzeeze@yahoo.com>
2026-01-30 06:15:54 -0800

    Add cards list array in script.js

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const initialCards = [{
+    name: "Val Thorens",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg"
+  },
+    {
+    name: "Restaurant terrace",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg"
+    },
+    {
+    name: "An outdoor cafe",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg"
+    },
+    {
+    name: "A very long bridge, over the forest and through the trees",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg"
+    },
+    {
+    name: "Tunnel with morning light",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg"
+    },
+    {
+    name: "Mountain house",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg"
+    }
+  ]
   //Profile elements
   const profileSection = document.querySelector(".profile");
   const editButton = profileSection.querySelector(".profile__edit-btn");
@@ -49,6 +74,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
   function handlePostModalForm(e) {
     e.preventDefault();
+    console.log(modalLinkInput.value);
+    console.log(modalCaptionInput.value);
+     closeModal(postModal);
 }
 
   editCloseButton.addEventListener("click", () => closeModal(editModal));
@@ -60,5 +88,7 @@ document.addEventListener("DOMContentLoaded", () => {
   editModalForm.addEventListener("submit", handleEditModalForm);
   postModalForm.addEventListener("submit", handlePostModalForm);
 
-
+    initialCards.forEach((item) => {
+      console.log(item.name, item.link);
+     })
 });
